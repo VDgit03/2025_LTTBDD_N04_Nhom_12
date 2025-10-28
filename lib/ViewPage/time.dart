498765234ac81244652b9766bbf4ea041a279e96
phuong12-phuong12nhim <nguyenphuongw1229@gmail.com>
2025-10-28 13:25:54 +0700

    chỉnh sửa file page.dart và thêm các hàm của file time.dart

---
+++ lib/ViewPage/time.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class Time extends StatefulWidget {
+  const Time({super.key});
+
+  @override
+  State<Time> createState() => _TimeState();
+}
+
+class _TimeState extends State<Time> {
+  //chuoi thoi gian hien tai
+  late String _timeString = '';
+  //
+  Timer? _timer;
+
+  //khoi tao thoi gian
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+  }
+
+  // ham cap nhat thoi gian
+  void _updateTime() {
+    final String formattedDateTime = _formatDateTime(DateTime.now());
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  // ham lay thoi gian
+  void _getTime() {
+    if (!mounted) return; // kiem tra an toan khi goi setState
+    final String formattedDateTime = _formatDateTime(DateTime.now());
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  // ham dinh dang thoi gian
+  String _formatDateTime(DateTime dateTime) {
+    return "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}";
+  }
+
+  //ham xoa thoi gian
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.lightBlueAccent,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.access_time, size: 32),
+          SizedBox(width: 8),
+          Text(
+            _timeString,
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/page.dart
+++ lib/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_ck/ViewPage/time.dart';
 
 class page extends StatelessWidget {
   const page({super.key});
@@ -11,37 +12,51 @@ class page extends StatelessWidget {
           title: Text('Page 1', textAlign: TextAlign.center),
           toolbarHeight: 100,
           backgroundColor: Colors.blueGrey,
+          centerTitle: true,
         ),
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Column(
-                children: [
-                  Container(height: 200, color: Colors.red),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(height: 100, color: Colors.red),
+              SizedBox(height: 20),
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Time(),
+                          SizedBox(height: 20),
 
-                  const SizedBox(height: 20),
-                  Container(height: 200, color: Colors.green),
-                ],
-              ),
-            ),
-            const SizedBox(width: 20),
+                          const SizedBox(height: 20),
+                          Container(height: 200, color: Colors.green),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 20),
 
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(height: 150, color: Colors.blue),
-                  const SizedBox(height: 20),
-                  Container(height: 150, color: Colors.yellow),
-                  const SizedBox(height: 20),
-                  Container(height: 150, color: Colors.orange),
-                ],
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(height: 150, color: Colors.blue),
+                          const SizedBox(height: 20),
+                          Container(height: 150, color: Colors.yellow),
+                          const SizedBox(height: 20),
+                          Container(height: 150, color: Colors.orange),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
