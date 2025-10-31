2b95fad0310c52f81aafed30b15bb706cb1e58eb
VDgit03 <dangvietdung2903@gmail.com>
2025-10-31 15:30:54 +0700

    thêm 2 file app_en và app_vi để thay đổi ngôn ngữ

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,81 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get username => 'Username';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get text_register => 'Login';
+
+  @override
+  String get button_register => 'Register';
+
+  @override
+  String get register_successful => 'Registration Successful';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get text_login => 'Register';
+
+  @override
+  String get button_login => 'Login';
+
+  @override
+  String get login_successful => 'Login successful';
+
+  @override
+  String get login_incorrect => 'Incorrect username or password! Please try again';
+
+  @override
+  String get homepage => 'Dashboard';
+
+  @override
+  String get text_db => 'Dashboard';
+
+  @override
+  String hello(String username) {
+    return 'Hello, $username';
+  }
+
+  @override
+  String get list => 'To Do List';
+
+  @override
+  String get settings => 'Setting';
+
+  @override
+  String get text_st => 'Setting';
+
+  @override
+  String get about => 'About us';
+
+  @override
+  String get text_about => 'About Us';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get text_logout => 'Logout Successful';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get theme => 'Theme';
+}
---
+++ lib/l10n/app_localizations_vi.dart
@@ -0,0 +1,81 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Vietnamese (`vi`).
+class AppLocalizationsVi extends AppLocalizations {
+  AppLocalizationsVi([String locale = 'vi']) : super(locale);
+
+  @override
+  String get username => 'Tên đăng nhập';
+
+  @override
+  String get password => 'Mật khẩu';
+
+  @override
+  String get register => 'Đăng kí';
+
+  @override
+  String get text_register => 'Đã có tài khoản? Đăng nhập ngay';
+
+  @override
+  String get button_register => 'Đăng kí';
+
+  @override
+  String get register_successful => 'Đăng kí thành công';
+
+  @override
+  String get login => 'Đăng nhập';
+
+  @override
+  String get text_login => 'Chưa có tài khoản? Đăng kí ngay';
+
+  @override
+  String get button_login => 'Đăng nhập';
+
+  @override
+  String get login_successful => 'Đăng nhập thành công';
+
+  @override
+  String get login_incorrect => 'Sai tên đăng nhập hoặc mật khẩu! Vui lòng thử lại';
+
+  @override
+  String get homepage => 'Trang chủ';
+
+  @override
+  String get text_db => 'Đã tới trang chủ';
+
+  @override
+  String hello(String username) {
+    return 'xin chào, $username';
+  }
+
+  @override
+  String get list => 'Danh sách công việc';
+
+  @override
+  String get settings => 'Cài đặt';
+
+  @override
+  String get text_st => 'Đã tới cài đặt';
+
+  @override
+  String get about => 'Giới thiệu';
+
+  @override
+  String get text_about => 'Giới thiệu về app';
+
+  @override
+  String get logout => 'Đăng xuất';
+
+  @override
+  String get text_logout => 'Đăng xuất thành công';
+
+  @override
+  String get language => 'Ngôn ngữ';
+
+  @override
+  String get theme => 'Giao diện';
+}
